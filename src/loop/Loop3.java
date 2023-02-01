28ebad0cfdc3eb15a9e2f0703cef75fc30f882e3
Sumeyra <smyryalcin@gmail.com>
2023-02-01 10:33:57 +0000

    creted new loop task printing even number

---
+++ src/loop/Loop3.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class Loop3 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 51; i++) {
+            if (i%2 == 0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
+/*
+Printing even numbers between 1 and 50
+ */
