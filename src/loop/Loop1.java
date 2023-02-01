4efe35a0f4ba71f47ec916f7490776c531f3df85
Sumeyra <smyryalcin@gmail.com>
2023-02-01 10:23:52 +0000

    creted new loop task

---
+++ src/loop/Loop1.java
@@ -0,0 +1,17 @@
+package loop;
+
+public class Loop1 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        for (int i = 0; i < 100; i++) {
+            count +=i;
+
+        }
+
+        System.out.println(count);
+    }
+}
+/*
+Summing up numbers from 1 to 100:
+ */
