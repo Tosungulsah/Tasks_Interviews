0e19b1e601e239977afca1e967bc776e48375c4a
Sumeyra <smyryalcin@gmail.com>
2023-02-01 10:29:08 +0000

    creted new loop task printing each array

---
+++ src/loop/Loop2.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class Loop2 {
+    public static void main(String[] args) {
+        String[] arr = {"a","b","c","d","e","f","g"};
+
+        for (String each : arr) {
+            System.out.print(each +" ");
+
+        }
+    }
+}
+/*
+Iterating over an array of strings and printing each element:
+ */
