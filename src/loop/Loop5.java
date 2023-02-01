438b1276a4cc5b599e55ddf0c0a9f631f0a354c3
Sumeyra <smyryalcin@gmail.com>
2023-02-01 11:00:07 +0000

    creted new loop task printing vowels in a string updated

---
+++ src/loop/Loop5.java
@@ -0,0 +1,28 @@
+package loop;
+
+public class Loop5 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 101; i++) {
+
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.print("FizzBuzz   ");
+            } else if (i% 5 == 0){
+                System.out.print("Buzz  ");
+            }else if (i% 3 == 0) {
+                System.out.print("Fizz  ");
+
+            }else {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
+/*
+Question: Write a Java program that prints the numbers from 1 to 100,
+ but for multiples of 3, it prints "Fizz" instead of the number,
+ and for multiples of 5, it prints "Buzz".
+ For numbers that are multiples of both 3 and 5, it should print "FizzBuzz".
+
+ */
