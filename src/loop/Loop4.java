4e62ec6d69f9e6d8be2a9116ab2b2badd67af9de
Sumeyra <smyryalcin@gmail.com>
2023-02-01 10:47:38 +0000

    creted new loop task printing vowels in a string

---
+++ src/loop/Loop4.java
@@ -0,0 +1,21 @@
+package loop;
+
+public class Loop4 {
+    public static void main(String[] args) {
+        String str = "ajhxjcsdjfleoea";
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if(c =='a' || c == 'u' || c =='e' || c =='i' || c =='o' ){
+                count ++;
+
+            }
+        }
+        System.out.println(count);
+    }
+}
+/*
+Counting the number of vowels in a string
+ */
